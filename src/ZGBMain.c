168fee5d36ab615da148523ab42ef48d12d2c91c
Zalo <cebolleto@gmail.com>
2017-04-08 13:41:01 +0200

    Init functions moved to bank 1

--- src/ZGBMain.c
+++ src/ZGBMain.c
@@ -1,24 +1,5 @@
 #include "ZGBMain.h"
 
-#include "StateGame.h"
-
-#include "SpritePlayer.h"
-
-#include "../res/src/player.h"
-
-UINT8 next_state = STATE_GAME;
-
-SET_N_STATES(N_STATES);
-SET_N_SPRITE_TYPES(N_SPRITE_TYPES);
-
-void InitStates() {
-	INIT_STATE(STATE_GAME);
-}
-
-void InitSprites() {
-	INIT_SPRITE(SPRITE_PLAYER, player, 3, FRAME_16x16, 1);
-}
-
 #include "Math.h"
 UINT8 GetTileReplacement(UINT8* tile_ptr, UINT8* tile) {
 	if(current_state == STATE_GAME) {
