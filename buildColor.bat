1f04b52e865315e7b96595a1ade28ba165e43e21
Zalo <cebolleto@gmail.com>
2017-04-11 18:49:52 +0200

    added bat files for cmd

---
+++ buildColor.bat
@@ -0,0 +1,5 @@
+C:\gbdk\bin\make-3.81-bin\bin\make -C %ZGB_PATH%\src BUILD_TYPE=ReleaseColor
+cd src
+C:\gbdk\bin\make-3.81-bin\bin\make run BUILD_TYPE=ReleaseColor
+cd ..
+pause
--- clean.bat
+++ clean.bat
@@ -1,5 +1,11 @@
-C:\gbdk\bin\make-3.81-bin\bin\make -C %ZGB_PATH%\src clean
+C:\gbdk\bin\make-3.81-bin\bin\make -C %ZGB_PATH%\src clean BUILD_TYPE=Release
+C:\gbdk\bin\make-3.81-bin\bin\make -C %ZGB_PATH%\src clean BUILD_TYPE=Debug
+C:\gbdk\bin\make-3.81-bin\bin\make -C %ZGB_PATH%\src clean BUILD_TYPE=ReleaseColor
+C:\gbdk\bin\make-3.81-bin\bin\make -C %ZGB_PATH%\src clean BUILD_TYPE=DebugColor
 cd src
-C:\gbdk\bin\make-3.81-bin\bin\make clean
+C:\gbdk\bin\make-3.81-bin\bin\make clean BUILD_TYPE=Release
+C:\gbdk\bin\make-3.81-bin\bin\make clean BUILD_TYPE=Debug
+C:\gbdk\bin\make-3.81-bin\bin\make clean BUILD_TYPE=ReleaseColor
+C:\gbdk\bin\make-3.81-bin\bin\make clean BUILD_TYPE=DebugColor
 cd ..
 pause
