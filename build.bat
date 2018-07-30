6a1ee9c81a14a69f1e58fa6bf3386312efa996c4
Zalo <cebolleto@gmail.com>
2018-07-30 19:53:46 +0200

    bat files removing build calls to zgb

--- build.bat
+++ build.bat
@@ -1,4 +1,3 @@
-%ZGB_PATH%\..\env\make-3.81-bin\bin\make -C %ZGB_PATH%\src 
 cd src
 %ZGB_PATH%\..\env\make-3.81-bin\bin\make run
 cd ..
--- clean.bat
+++ clean.bat
@@ -1,7 +1,3 @@
-%ZGB_PATH%\..\env\make-3.81-bin\bin\make -C %ZGB_PATH%\src clean BUILD_TYPE=Release
-%ZGB_PATH%\..\env\make-3.81-bin\bin\make -C %ZGB_PATH%\src clean BUILD_TYPE=Debug
-%ZGB_PATH%\..\env\make-3.81-bin\bin\make -C %ZGB_PATH%\src clean BUILD_TYPE=ReleaseColor
-%ZGB_PATH%\..\env\make-3.81-bin\bin\make -C %ZGB_PATH%\src clean BUILD_TYPE=DebugColor
 cd src
 %ZGB_PATH%\..\env\make-3.81-bin\bin\make clean BUILD_TYPE=Release
 %ZGB_PATH%\..\env\make-3.81-bin\bin\make clean BUILD_TYPE=Debug
