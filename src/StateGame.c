d978b9bc13b6c327ac84d1c712ec82f3c30761db
Zal0 <cebolleto@gmail.com>
2021-09-12 10:39:08 +0200

    loading sprites on demand

--- src/StateGame.c
+++ src/StateGame.c
@@ -7,17 +7,8 @@
 IMPORT_MAP(map);
 
 void START() {
-	UINT8 i;
-
-	SPRITES_8x16;
-	for(i = 0; i != N_SPRITE_TYPES; ++ i) {
-		SpriteManagerLoad(i);
-	}
-	SHOW_SPRITES;
-
 	scroll_target = SpriteManagerAdd(SpritePlayer, 50, 50);
 	InitScroll(BANK(map), &map, 0, 0);
-	SHOW_BKG;
 }
 
 void UPDATE() {
